3c2a69f4b00b03b9fd074fc944f95c1c0f93822e
megatricycle <megatricycle@gmail.com>
2015-01-03 14:44:15 +0800

    fixed autocomplete not working in signup

--- routes/index.js
+++ routes/index.js
@@ -312,30 +312,24 @@ router.post('/signup',
 
 //search
 router.get('/search',function(req,res){
-	var session = req.session;
-	if(session.userkey){
-		pool.getConnection(function(err,connection){
-			var query = "SELECT first_name, full_name AS 'value', picture from `accounts` WHERE 1 ORDER BY full_name";
+	pool.getConnection(function(err,connection){
+		var query = "SELECT first_name, full_name AS 'value', picture from `accounts` WHERE 1 ORDER BY full_name";
 
-			connection.query(query,function(err,rows){
-				if(err){
-					console.log(err);
-					res.send("Internal server error");
+		connection.query(query,function(err,rows){
+			if(err){
+				console.log(err);
+				res.send("Internal server error");
+			}
+			else{
+				for(var i = 0; i < rows.length; i++){
+					rows[i]["picture"] = rows[i]["picture"].substring(7);
+					rows[i]["url"] = "http://localhost:8080/profile/" + rows[i]["first_name"];
 				}
-				else{
-					for(var i = 0; i < rows.length; i++){
-						rows[i]["picture"] = rows[i]["picture"].substring(7);
-						rows[i]["url"] = "http://localhost:8080/profile/" + rows[i]["first_name"];
-					}
-					res.send(rows);
-				}
-			});
-			connection.release()
+				res.send(rows);
+			}
 		});
-	}
-	else{
-		res.redirect('/');
-	}
+		connection.release()
+	});
 });
 
 //view all YSERs
