14621b98dd83356cfcc69659db266d391dd52d99
atmalmer18 <atmalmer23@gmail.com>
2015-01-07 02:37:05 +0800

    Department button finalized.

--- public/javascripts/department.js
+++ public/javascripts/department.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	$("a.menu-icon-department").click(function(e) {
 		e.preventDefault();
 		if($(this).closest('#header').find('#department-button-container').is(":visible"))
@@ -6,6 +7,15 @@ $(document).ready(function(){
 		else
 			$(this).closest('#header').find('#department-button-container').slideDown();
 	});
+});
 
+$(document).mouseup(function (e){
+    var container = $("#department-button-container");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.slideUp();
+    }
 });
  
--- public/javascripts/title-hover.js
+++ public/javascripts/title-hover.js
@@ -1,9 +1,19 @@
 $(document).ready( function() {
 
-    $('.gallery-item').hover( function() {
-        $(this).find('.img-title').fadeIn(300);
-    }, function() {
-        $(this).find('.img-title').fadeOut(100);
-    });
-	
+    $('.gallery-item').hover( 
+	    function() {
+	    	$(this).find('ul').slideDown(500);
+	    }, function() {
+	    	$(this).find('ul').slideUp(300);
+	    }
+    );
+
+    $('.gallery-item').hover( 
+	    function() {
+	        $(this).find('.img-title').fadeIn(200);
+	    }, function() {
+	        $(this).find('.img-title').fadeOut(100);
+	    }
+    );
+
 });
