d3de3d047308df2d81bfbe2418bd52ab6656634a
megatricycle <megatricycle@gmail.com>
2015-01-18 00:20:36 +0800

    backend functionality for jpad scores for jpad users with statistics

--- routes/pad/jpad/jpadscores.js
+++ routes/pad/jpad/jpadscores.js
@@ -11,7 +11,7 @@ module.exports = function(app,pool,async){
 
     if(session.userkey){
       pool.getConnection(function(err,connection){
-				//be sure user is an SPAD member before doing stuffs!
+				//be sure user is a JPAD member before doing stuffs!
 				var query = "SELECT department FROM `accounts` WHERE username="+connection.escape(session.userkey);
 
 				connection.query(query,function(err,department){
@@ -21,7 +21,44 @@ module.exports = function(app,pool,async){
 							res.sendStatus(403);
 						}
 						else{
+							async.waterfall([
+								function(callback){
+									//fetch list of tasks
+									var query = "SELECT `task`, DATE_FORMAT(`date`,'%c/%e/%Y') AS date, `maxscore` FROM pad_jpadsters_task WHERE 1 ORDER BY date DESC";
 
+									connection.query(query,function(err,tasks){
+										if(err) callback(err);
+										else{
+											callback(null,tasks);
+										}
+									});
+								},
+								function(tasks,callback){
+									//fetch score for each tasks
+									async.each(tasks,
+									function(task,callback){
+										//query the database for score
+										var query = "SELECT `score` FROM pad_jpadsters_scores WHERE username="+connection.escape(session.userkey)+" AND task="+connection.escape(task["task"]);
+
+										connection.query(query,function(err,score){
+											if(err) callback(err);
+											else{
+												//add score property on task with the score data as value
+												task["score"] = score[0]["score"];
+												callback();
+											}
+										});
+									},
+									function(err){
+										callback(null,tasks);
+									});
+								}
+							],
+							function(err,send){
+								//render page with data!
+								if(err) reportError(res,err);
+								res.render('jpadscores',{scores:send});
+							});
 						}
 					}
 				});
